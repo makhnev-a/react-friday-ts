894aa36489a887b18468bf07d8bd6cb779611ea6
makhnev-a <ray.makhnev@gmail.com>
2020-07-24 17:53:35 +0300

    Updates

---
+++ precache-manifest.0d219551f91406e87acebf55f8c257fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327f2355345f24ccdd7cc2799a6d549d",
+    "url": "/react-friday-ts/index.html"
+  },
+  {
+    "revision": "75e040f0144a0386d159",
+    "url": "/react-friday-ts/static/css/main.ad2953e3.chunk.css"
+  },
+  {
+    "revision": "02da03bf5cffd226364c",
+    "url": "/react-friday-ts/static/js/2.56657682.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-friday-ts/static/js/2.56657682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e040f0144a0386d159",
+    "url": "/react-friday-ts/static/js/main.ab65a394.chunk.js"
+  },
+  {
+    "revision": "5a884fae1dbc5004c14c",
+    "url": "/react-friday-ts/static/js/runtime-main.ccb8431c.js"
+  }
+]);
