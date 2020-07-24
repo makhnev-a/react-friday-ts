2b657748588242ef1c4be14cdbb3d88947bce0c2
makhnev-a <ray.makhnev@gmail.com>
2020-07-24 15:25:46 +0300

    Init

---
+++ src/components/NewPassword/NewPassword.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const NewPassword = () => {
+    return (
+        <h1>NewPassword page</h1>
+    );
+};
---
+++ src/components/PasswordRecovery/PasswordRecovery.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const PasswordRecovery = () => {
+   return (
+       <h1>PasswordRecovery page</h1>
+   );
+};
