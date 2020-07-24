005a7c329bb678696281a5e73e8b23d4977af3f3
makhnev-a <ray.makhnev@gmail.com>
2020-07-24 17:25:09 +0300

    moved the routes into a separate file

---
+++ src/utils/helpers/routes.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Route} from "react-router-dom";
+import {Login} from "../../pages/Login/Login";
+import {Registration} from "../../pages/Registration/Registration";
+import {NewPassword} from "../../pages/NewPassword/NewPassword";
+import {PasswordRecovery} from "../../pages/PasswordRecovery/PasswordRecovery";
+import {Profile} from "../../pages/Profile/Profile";
+
+export const AppRoutes = () => {
+    return (
+        <>
+            <Route path='/login' render={Login}/>
+            <Route path='/registration' render={Registration}/>
+            <Route path='/new-pass' render={NewPassword}/>
+            <Route path='/restore-pass' render={PasswordRecovery}/>
+            <Route path='/profile' render={Profile}/>
+        </>
+    );
+};
