742d6a215e52052ae65e261d444cf2ec045456da
makhnev-a <ray.makhnev@gmail.com>
2020-07-31 00:04:09 +0300

    fix bug with hooks

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {Input} from "../../utils/Input/Input";
 import {Button} from "../../utils/Button/Button";
 
-export const Login = () => {
+export const Login: React.FC = () => {
     return (
         <>
             <h1>Login page</h1>
             <form>
-                <Input type={'text'} placeholder={'Enter you email'} />
-                <Input type={'password'} placeholder={'Enter you password'} />
-                <Button text={'login'} />
+                <Input type={'text'} placeholder={'Enter you email'}/>
+                <Input type={'password'} placeholder={'Enter you password'}/>
+                <Button text={'login'}/>
             </form>
         </>
     );
